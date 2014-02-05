a1f4f0627c83bc9564a4f27329b1a77b104ce366
scheruku <priyanka.cherukumilli@gmail.com>
2014-02-05 18:21:55 -0500

    my second test with 2 files

--- application/views/Aboutme.php
+++ application/views/Aboutme.php
@@ -17,7 +17,7 @@ background-position: 0 40px;
 	<img src=""/>
 	<img class="img-responsive" src="<?echo base_url().'/images/profile_1.jpg';?>" alt="...">
 		
-		<p>Hey!  I am Priyanka Cherukumilli a software developer by profession. I am fascinated by software development 
+		<p>Hey!  I am testing second time. I am Priyanka Cherukumilli a software developer by profession. I am fascinated by software development 
 		and how it evolved through the years and made huge difference in our lives. In my spare time I enjoy learning new coding techniques, exploring open source projects and new technologies.
 		</p>
 		<p>I love cooking and often experiment new cuisines. I am a trained in Indian classical music and in my spare time I enjoy singing and hiking.
